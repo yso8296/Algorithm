3406b02cde6a54bd9a3d1d36d6d71a23ca9c5650
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-02-15 14:48:54 +0900

    [ETC] 두 배열의 원소 교체

---
+++ Algorithm/ETC/정렬/두 배열의 원소 교체.py
@@ -0,0 +1,11 @@
+n, k = map(int, input().split())
+array1 = list(map(int, input().split()))
+array2 = list(map(int, input().split()))
+array1.sort()
+array2.sort(reverse=True)
+
+for i in range(k):
+  if array1[i] < array2[i]:
+    array1[i], array2[i] = array2[i], array1[i]
+
+print(sum(array1))
