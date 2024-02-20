668ffecd54a864f8a8ff5e21438ed0d3ba4ed40f
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-02-20 15:08:38 +0900

    [ETC] 전보

---
+++ Algorithm/ETC/최단 경로/전보.py
@@ -0,0 +1,32 @@
+import heapq
+INF = int(1e9)
+n, m, start = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+distance = [INF] * (n + 1)
+for _ in range(m):
+  a, b, c = map(int, input().split())
+  graph[a].append((b, c))
+
+def dijkstra(start):
+  q = []
+  heapq.heappush(q, (0, start))
+  distance[start] = 0
+  while q:
+    dist, now = heapq.heappop(q)
+    if distance[now] < dist:
+      continue
+    for i in graph[now]:
+      cost = dist + i[1]
+      if cost < distance[i[0]]:
+        distance[i[0]] = cost
+        heapq.heappush(q, (cost, i[0]))
+
+dijkstra(start)
+count = 0
+dist = 0
+for d in distance:
+  if d != INF:
+    count += 1
+    dist = max(dist, d)
+
+print(count - 1, dist)
