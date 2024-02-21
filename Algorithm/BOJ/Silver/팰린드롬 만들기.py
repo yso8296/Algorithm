8741e3251341cd593597f7e3bffc60164c45beda
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-02-21 13:04:30 +0900

    [BOJ] 팰린드롬 만들기

---
+++ Algorithm/BOJ/Silver/팰린드롬 만들기.py
@@ -0,0 +1,17 @@
+from collections import Counter
+word = list(input())
+check_word = Counter(word)
+cnt = 0
+result = ''
+mid = ''
+for k, v in list(check_word.items()):
+    if v % 2 == 1: 
+        cnt += 1
+        mid = k 
+        if cnt >= 2: 
+            print("I'm Sorry Hansoo")
+            exit()
+
+for k, v in sorted(check_word.items()): 
+    result += (k * (v // 2)) 
+print(result + mid + result[::-1]) 
