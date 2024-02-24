3558de48a78eb280b884492a65c9b8c6e489b98d
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-02-24 15:40:16 +0900

    [BOJ] 연산자 끼워넣기

--- Algorithm/BOJ/Silver/연산자 끼워넣기.py
+++ Algorithm/BOJ/Silver/연산자 끼워넣기.py
@@ -1,33 +1,33 @@
 n = int(input())
-numbers = list(map(int, input().split()))
-operator = list(map(int, input().split())) # + - * //
+array = list(map(int, input().split()))
+operator = list(map(int, input().split()))
 max_num = -int(1e9)
 min_num = int(1e9)
 
-def calculator(num, count):
+def calculator(num, k):
   global max_num
   global min_num
-  if count == n:
+  if k == n:
     max_num = max(max_num, num)
     min_num = min(min_num, num)
     return
   if operator[0] != 0:
     operator[0] -= 1
-    calculator(num + numbers[count], count + 1)
+    calculator(num + array[k], k + 1)
     operator[0] += 1
   if operator[1] != 0:
     operator[1] -= 1
-    calculator(num - numbers[count], count + 1)
+    calculator(num - array[k], k + 1)
     operator[1] += 1
   if operator[2] != 0:
     operator[2] -= 1
-    calculator(num * numbers[count], count + 1)
+    calculator(num * array[k], k + 1)
     operator[2] += 1
   if operator[3] != 0:
     operator[3] -= 1
-    calculator(int(num / numbers[count]), count + 1)
+    calculator(int(num / array[k]), k + 1)
     operator[3] += 1
-  
-calculator(numbers[0], 1)
+
+calculator(array[0], 1)
 print(max_num)
 print(min_num)
