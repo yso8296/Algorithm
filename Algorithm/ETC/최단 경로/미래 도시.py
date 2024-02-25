70fe337d53b9aa3510093e3833e4a465d67dd971
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-02-25 16:27:56 +0900

    [ETC] 미래 도시

--- Algorithm/ETC/최단 경로/미래 도시.py
+++ Algorithm/ETC/최단 경로/미래 도시.py
@@ -1,25 +1,23 @@
 n, m = map(int, input().split())
 INF = int(1e9)
 graph = [[INF] * (n + 1) for _ in range(n + 1)]
-for i in range(1, n + 1):
-  for j in range(1, n + 1):
+for _ in range(m):
+  x, y = map(int, input().split())
+  graph[x][y] = 1
+  graph[y][x] = 1
+a, b = map(int, input().split())
+for i in range(n):
+  for j in range(n):
     if i == j:
       graph[i][j] = 0
 
-for _ in range(m):
-  a, b = map(int, input().split())
-  graph[a][b] = 1
-  graph[b][a] = 1
-
 for k in range(1, n + 1):
-  for a in range(1, n + 1):
-    for b in range(1, n + 1):
-      graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+  for i in range(1, n + 1):
+    for j in range(1, n + 1):
+      graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
 
-x, k = map(int, input().split())
-
-distance = graph[1][k] + graph[k][x]
-if distance >= INF:
+result = graph[1][b] + graph[b][a]
+if result >= INF:
   print(-1)
 else:
-  print(distance)
+  print(result)
