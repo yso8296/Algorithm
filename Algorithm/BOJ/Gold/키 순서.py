e947ffe3b9bcfae3c526010933ce59f66d04c77c
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-02-27 11:53:25 +0900

    [BOJ] 키 순서

---
+++ Algorithm/BOJ/Gold/키 순서.py
@@ -0,0 +1,25 @@
+n, m = map(int, input().split())
+INF = int(1e9)
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
+  for j in range(1, n + 1):
+    if i == j:
+      graph[i][j] = 0
+for _ in range(m):
+  a, b = map(int, input().split())
+  graph[a][b] = 1
+
+for k in range(1, n + 1):
+  for i in range(1, n + 1):
+    for j in range(1, n + 1):
+      graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+result = 0
+for i in range(1, n + 1):
+  count = 0
+  for j in range(1, n + 1):
+    if graph[i][j] != INF or graph[j][i] != INF:
+      count += 1
+  if count == n:
+    result += 1
+print(result)
