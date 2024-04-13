9bad8ea2a2dfe831eb39a5c0e7de4491f913e34b
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-04-14 00:53:50 +0900

    [BOJ] 부분수열의 합

---
+++ Algorithm/BOJ/Silver/부분수열의 합.py
@@ -0,0 +1,12 @@
+from itertools import combinations
+n, s = map(int, input().split())
+arr = list(map(int, input().split()))
+count = 0
+
+for i in range(1, n + 1):
+  comb = combinations(arr, i)
+
+  for j in comb:
+    if sum(j) == s:
+      count += 1
+print(count)
