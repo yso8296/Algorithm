e1a016d9cf238741b55e5e3e8ce62005edf9fbf9
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-04-20 22:15:05 +0900

    [BOJ] 스타트와 링크

--- Algorithm/BOJ/Silver/스타트와 링크.py
+++ Algorithm/BOJ/Silver/스타트와 링크.py
@@ -1,38 +1,34 @@
 n = int(input())
-board = []
+graph = []
 for _ in range(n):
-  board.append(list(map(int, input().split())))
-num = n // 2
+  graph.append(list(map(int, input().split())))
+start = []
+link = []
 result = int(1e9)
-visited = [False] * n
-
-def simulate(team):
-  start = []
-  link = []
-  start_sum = 0
-  link_sum = 0
-  for i in range(n):
-    if team[i]:
-      start.append(i)
-    else:
-      link.append(i)
-  for i in range(num):
-    for j in range(num):
-      if i != j:
-        start_sum += board[start[i]][start[j]] 
-        link_sum += board[link[i]][link[j]]
-  return abs(start_sum - link_sum)
 
 def dfs(count, idx):
   global result
-  if count == num:
-    result = min(result, simulate(visited))
-
+  if count == int(n / 2):
+    start_sum = 0
+    link_sum = 0
+    for i in range(n):
+      if i not in start:
+        link.append(i)
+    for i in range(int(n / 2) - 1):
+      for j in range(i + 1, int(n / 2)):
+        start_sum += (graph[start[i]][start[j]] + graph[start[j]][start[i]])
+    for i in range(int(n / 2) - 1):
+      for j in range(i + 1, int(n / 2)):
+        link_sum += (graph[link[i]][link[j]] + graph[link[j]][link[i]])
+    result = min(result, abs(start_sum - link_sum))
+    link.clear()
+    return
+  
   for i in range(idx, n):
-    if not visited[i]:
-      visited[i] = True
+    if i not in start:
+      start.append(i)
       dfs(count + 1, i + 1)
-      visited[i] = False
-      
+      start.pop()
+
 dfs(0, 0)
 print(result)
